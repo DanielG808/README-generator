4d68f16d8c0e553dfe864b737f5e515767837a0d
Dan G <daniel.w.gray1994@gmail.com>
2023-06-08 20:14:51 -0400

    project complete!

---
+++ README.md
@@ -0,0 +1,39 @@
+# The README.md Generator
+
+# Table of Contents
+    
+[Description](#description)
+<br>[Installation](#installation)
+<br>[Usage](#usage)
+<br>[License](#license)
+<br>[Contributing](#contributing)
+<br>[Test](#tests)
+<br>[Questions](#questions)
+    
+# Description
+    
+This is a README.md Generator! After finishing all of the prompts, The README.md Generator will dynamically create a README.md based on the user responses.
+    
+# Installation
+    
+In order to install The README.md Generator, the user will need to node.js installed as well as npm and the inquirer.js package.
+    
+# Usage
+    
+To contribute please reach out to me via the contact info below or submit a pull request to this repo!
+    
+# License
+    
+MIT
+    
+# Contributing
+    
+To contribute please reach out to me via the contact info below or submit a pull request to this repo!
+    
+# Tests
+    
+The application can be tested by running the command 'node index' in the console while the user is currently cd'd into the directly containing the index.js file.
+    
+# Questions
+    
+Feel free to reach out with any questions at my [GitHub](https://github.com/DanielG808) or contact me via email at stjimmy400@gmail.com
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// DEPENDENCIES
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// DATA
 
 const questions = [
     {
@@ -26,11 +29,83 @@ const questions = [
         type: 'input',
         message: 'Tell users how to test if the application is working:',
         name: 'tests'
+    },
+    {
+        type: 'list',
+        message: 'Which license would you like to use?',
+        choices: ['MIT', 'Other', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause', 'Unlicense', 'BSD 2-clause', 'LGPLv3', 'AGPLv3'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Add a link to your GitHub profile for questions from users:',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Add your email address for questions from users:',
+        name: 'email'
     }
 ]
 
-inquirer
+// FUNCTIONS
+
+const handleAnswers = ({ title, description, installation, contributing, tests, license, github, email }) => {
+    const data = 
+    
+`# ${title}
+
+# Table of Contents
+    
+[Description](#description)
+<br>[Installation](#installation)
+<br>[Usage](#usage)
+<br>[License](#license)
+<br>[Contributing](#contributing)
+<br>[Test](#tests)
+<br>[Questions](#questions)
+    
+# Description
+    
+${description}
+    
+# Installation
+    
+${installation}
+    
+# Usage
+    
+${contributing}
+    
+# License
+    
+${license}
+    
+# Contributing
+    
+${contributing}
+    
+# Tests
+    
+${tests}
+    
+# Questions
+    
+Feel free to reach out with any questions at my [GitHub](${github}) or contact me via email at ${email}`
+
+    fs.writeFileSync("./README.md", data);
+    console.log("Success! Your file has been created.")
+}
+
+function init() {
+    console.log("Welcome to he README.md Generator!")
+    inquirer
     .prompt(questions)
-    .then((response) =>
-        console.log(response)
-    )
+    .then(handleAnswers); 
+}
+
+
+// USER INTERACTIONS
+
+// INITIALIZATIONS
+init();
